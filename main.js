c08bade17ddcdb00580864191507316aea7bac22
unknown <nathan.hlungwani123@gmail.com>
2024-07-08 22:52:17 +0200

    added added function for adding trip and displaying it as a table formate on innerHtml

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let data = JSON.parse(localStorage.getItem('data')) || [];
     // let data = []
     loadTasksFromLocStorage(); // load tasks from local storage 
-
+    readAll(); 
    
 
     function loadTasksFromLocStorage(){ //create load tasks function
@@ -12,3 +12,39 @@ let data = JSON.parse(localStorage.getItem('data')) || [];
         }
      
      }
+
+
+
+     // function to read the data and show in inner html 
+function readAll() {
+    let tData = document.getElementById('table_data');
+    let elements = "";
+    data.forEach((d, index) => {
+        elements += `
+            <table class="table">
+        <thead>
+            
+                <th>From</th>
+                <th>To</th>
+                <th>Amount</th>
+                <th>Remove</th>
+        </thead>
+        <tbody id="table_data">
+
+         <tr>
+            <td>${d.pointA}</td>
+            <td>${d.pointB}</td>
+            <td>${d.amount}</td>
+            <td><button onclick="del(${index})">Delete</button></td>
+          </tr>
+
+        </tbody>
+
+    </table>
+
+        
+       `;
+    });
+    tData.innerHTML = elements;
+    
+}
